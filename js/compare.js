c212831b8a1545a4ee93d745e360410f7f96e717
Erica Pramer <erica.pramer@gatech.edu>
2016-05-02 08:08:57 -0400

    added timeline to compare mode

--- js/compare.js
+++ js/compare.js
@@ -19,10 +19,15 @@ document.createSvg = function(tagName) {
 
     showData(d);
 
+    drawTimeline();
+
 
 
 	})
 
+var arrayColors = ["#720042", "#BA390B", "#6EA2A3", "#336531","#D98634","#A34F93","#9FBCBF","#9BCF7A","#CC7172","#CFB47E"]
+var countryNames = ["England", "Spain", "France"];
+var numColors = countryNames.length;
   var makeGrid = function(boxesPerSide, size, pixelsPerSide, currYearID){ //TODO: handle edge cases for specific yearboxes
 
     //whole svg 
@@ -186,6 +191,196 @@ document.createSvg = function(tagName) {
         document.getElementById('internalData').innerHTML = dataList + '<li>' + dataEntry + '</li>';
     })
   }
+
+  function drawTimeline() {
+    var margin= {top:60, bottom:20, right:25, left:15};
+
+    document.getElementById("timelineCompare").innerHTML = ""; //clear out any previous timeline
+    
+    var dataArr = generateEventDataArray(10,0);
+
+    //object with key as year and value as the number of events during that year
+    var yearsMap = {};
+
+    var canvas = d3.select('#timelineCompare').append('svg')
+              .attr("width",document.getElementById("timelineCompare").offsetWidth); //current width of the timelineContainer div
+    
+    var timeline = canvas.append('g')
+                  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var xScale = d3.scale.linear()
+                  .domain([0, 99])
+                  .range([0,document.getElementById("timelineCompare").offsetWidth - margin.right])
+
+    var xAxis = d3.svg.axis()
+                  .scale(xScale)
+                  .orient("bottom")
+                  .ticks(100)
+                  .tickFormat(function(d) { //dont display text unless it's an even decade
+                    if((d % 10) != 0){ 
+                        return ("");
+                    }else{ 
+                        return (d + 1500); //the 1600 would be user input start century
+                    }});
+
+    var xGuide = canvas.append('g')
+                  .attr("transform", "translate(" + margin.left + "," + (margin.top + margin.bottom) + ")")
+                  .attr("class","axis")
+                  .style("stroke-width",2)
+                  .call(xAxis);
+
+    d3.selectAll("g.axis g.tick") //g.tick is the value of the tick
+      .style("stroke-width", function(d,i){
+        if(i%10 === 0)
+          return 2;
+        else
+          return 1;
+      });
+
+      d3.selectAll("g.axis g.tick line") //g.tick line is the actual tick mark
+      .attr("y2", function(d,i){
+        if(i%10 === 0)
+          return 10;
+        else
+          return 5;
+      });
+
+    timeline.selectAll("rect")
+      .data(dataArr)
+      .enter()
+      .append("rect")
+      .attr("x",function(d){return (xScale(+d.year)-3)})
+      .attr("y", function(d){ //prevent overlapping rectangles by keeping track of how many events occur each year
+        if(yearsMap[d.year] == null)
+            yearsMap[d.year] = 1;
+        else 
+            yearsMap[d.year] = +yearsMap[d.year] + 1;
+        return (19 - 7*yearsMap[d.year] - yearsMap[d.year])})
+      .attr("width", 7)
+      .attr("height", 7)
+      .attr("fill", function(d){return d.color})
+      .on("mouseover",function(d){ //show and hide tooltip of event label
+              document.getElementById(d.text + d.year).style.visibility = "visible";
+      })
+      .on("mouseout", function(d){
+        document.getElementById(d.text + d.year).style.visibility ="hidden";
+      });
+
+    timeline.selectAll("text") 
+      .data(dataArr)
+      .enter()
+      .append("text")
+      .text(function(d){return d.text})
+      .attr("x",function(d){return xScale(+d.year)}) //TODO: if year is 68, place text on left side of data point
+      .attr("y",5)
+      .attr("class","textLabels")
+      .attr("id", function(d){return d.text + d.year}) //allows text elements to be accessible by their corresponding rect
+      .style("visibility", "hidden");
+  };
+
+  /*this function pulls events from a chart and returns as an array of objects*/
+   function generateEventDataArray(boxesPerSide, yearID){ 
+    var timelineDataPts = []; //array of points to be plotted on the timeline
+      for(var i = 0; i < boxesPerSide; i++) {
+        for(var j = 0; j < boxesPerSide; j++) {
+          //document.getElementById("timeline").innerHTML = document.getElementById("timeline").innerHTML + yearID + "<br>"; //label for the year in which the events took place
+          for(var numType = 0; numType < 9; numType++){ //check each square for a fill
+            var typeSquare = document.getElementById("comparetype" + numType + "compareyear" + yearID);
+            var triangle = document.getElementById("comparetritype" + numType + "compareyear" + yearID); //the triangle occupying the type square. could be null
+            //for number of colors
+            for(var numClr = 1; numClr <= numColors; numClr++){ //check which color the fill is
+              if(triangle != null){
+                if(triangle.getAttribute("fill") == arrayColors[numClr-1])
+                {
+                  var country = countryNames[numClr-1];
+                  //9 if else statements for type of event. to avoid: would be nice to have an added attribute during makeGrid that is eventName
+                   if(numType == 0){
+                    //add data point object: year, color, text
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Beginning of war"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Beginning of war <br>"; //using the color as a key, gets the corresponding country value from countries
+                   }
+                   else if(numType == 1){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Conquest, annexation, or union"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Conquest, annexation, or union <br>"; 
+                   }
+                   else if(numType == 2){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Loss or disaster"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Loss or disaster <br>";
+                   }
+                   else if(numType == 3){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Fall of state"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Fall of state <br>";
+                   }
+                   else if(numType == 4){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Foundation or revolution"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Foundation or revolution <br>";
+                   }
+                   else if(numType == 5){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Treaty or sundry"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Treaty or sundry <br>";
+                   }
+                   else if(numType == 6){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Birth of remarkable individual"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Birth of remarkable individual <br>";
+                   }
+                   else if(numType == 7){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Deed"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Deed <br>"; 
+                   }
+                   else if(numType == 8){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Death of remarkable individual"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Death of remarkable individual <br>";
+                   }
+                } //end if triangle.getAttr
+              } //end if triangle != null
+              if(typeSquare.getAttribute("fill") == arrayColors[numClr-1])
+                {
+                  var country = countryNames[numClr-1];
+                  //9 if else statements for type of event. to avoid: would be nice to have an added attribute during makeGrid that is eventName
+                   if(numType == 0){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Beginning of war"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Beginning of war <br>"; //using the color as a key, gets the corresponding country value from countries
+                   }
+                   if(numType == 1){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Conquest, annexation, or union"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Conquest, annexation, or union <br>"; 
+                   }
+                   if(numType == 2){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Loss or disaster"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Loss or disaster <br>";
+                   }
+                   if(numType == 3){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Fall of state"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Fall of state <br>";
+                   }
+                   if(numType == 4){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Foundation or revolution"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Foundation or revolution <br>";
+                   }
+                   if(numType == 5){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Treaty or sundry"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Treaty or sundry <br>";
+                   }
+                   if(numType == 6){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Birth of remarkable individual"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Birth of remarkable individual <br>";
+                   }
+                   if(numType == 7){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Deed"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Deed <br>"; 
+                   }
+                   if(numType == 8){
+                    timelineDataPts.push({year: yearID, color: arrayColors[numClr-1], text: country + ": Death of remarkable individual"});
+                    //document.getElementById("timeline").innerHTML =  document.getElementById("timeline").innerHTML + country + ": Death of remarkable individual <br>";
+                   }
+                } //end if typeSquare
+            } //end for numClr
+          } //end for numType
+          yearID = yearID + 1;
+        }
+      }
+      return timelineDataPts;
+  };
 
   drawLine = function(x1,y1,x2,y2,group,strokeClr,dashWidth,dashSpace,strokeWidth){
   	var aLine = document.createSvg('line');
