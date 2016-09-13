4e05182490bb2e66428a76169dd481f675b8cc0d
Shivani Negi <negishivs@gmail.com>
2016-09-13 01:32:08 -0400

    creates node app and express server

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8000);
