0ea33ccf65afc537a8ca417e75c27d9b962dc837
Shivani Negi <negishivs@gmail.com>
2016-10-04 15:23:37 -0400

    add svg without numbers

--- js/view.js
+++ js/view.js
@@ -13,7 +13,7 @@ document.createSvg = function(tagName) {
 	d3.csv('peabodyData.csv', function(d){
 		var container = document.getElementById("viewGrid");
 		container.appendChild(makeGrid(480, 1)); //makes four 5x5 quadrant with boxes 30 px wide
-
+    makeTypeSvg();
 		/*populate chart*/
     fillChart(d);
 
@@ -21,6 +21,88 @@ document.createSvg = function(tagName) {
     fillEventList(d);
 
 	})
+
+  var makeTypeSvg = function(){
+    function gridData(){
+      var data = new Array();
+      var width = 30;
+      var height = 30;
+      var xpos = 1; //starting xpos and ypos at 1 so the stroke will show when we make the grid below
+      var ypos = 1;
+      var i = 1;
+
+      for (var row = 0; row < 3; row++){
+         data.push( new Array() );
+         for (var column = 0; column < 3; column++) {
+           data[row].push({
+                    x: xpos,
+                    y: ypos,
+                    width: width,
+                    height: height,
+                    number: i
+                })
+            xpos += width;
+            i++;
+        }
+                // reset the x position after a row is complete
+          xpos = 1;
+          // increment the y position for the next row. Move it down 50 (height variable)
+          ypos += height;
+
+
+      }
+      return data;
+    }
+      var gridData = gridData();
+
+      var grid = d3.select("div#typeGrid")
+    .append("svg")
+    .attr("width","100px")
+    .attr("height","100px");
+
+    var row = grid.selectAll(".row")
+  .data(gridData)
+  .enter().append("g")
+  .attr("class", "row");
+
+var column = row.selectAll(".square")
+  .data(function(d) { return d; })
+  .enter().append("rect")
+  .attr("class","square")
+  .attr("x", function(d) { return d.x; })
+  .attr("y", function(d) { return d.y; })
+  .attr("width", function(d) { return d.width; })
+  .attr("height", function(d) { return d.height; })
+  .style("fill", "#fff")
+  .style("stroke", "#222")
+  // .text(function(d) { return d.number.toString(); });
+
+  column.append("text")
+    .attr("x", function(d){return d.x +2})
+    .attr("y", function(d){return d.x +2})
+    .attr("dy", ".35em")
+    .text(function(d) { return d.number; });
+
+
+    // var row = grid.selectAll(".row")
+    // .data(gridData)
+    // .enter().append("g")
+    // .attr("class", "row");
+
+    // var column = row.selectAll(".square")
+    // .data(function(d) { return d; })
+    // .enter().append("rect")
+    // .attr("class","square")
+    // .attr("x", function(d) { return d.x; })
+    // .attr("y", function(d) { return d.y; })
+    // .attr("width", function(d) { return d.width; })
+    // .attr("height", function(d) { return d.height; })
+    // .style("fill", "#fff")
+    // .style("stroke", "#222");
+
+  }
+
+
 
   var makeGrid = function(size, currYearID){ //TODO: handle edge cases for specific yearboxes
       var centSize=size+gControl.lines.thick*3;
