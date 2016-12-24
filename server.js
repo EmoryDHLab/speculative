ad9bc672a2d68dd1c9e38148531beec41e76f848
Shivani Negi <negishivs@gmail.com>
2016-12-24 00:16:44 -0800

    two pages, route logic, and one button programmed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require('path');
 app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/zoomingui.html'));
+    res.sendFile(path.join(__dirname + '/new-ui.html'));
 });
 
 app.listen(8000);
