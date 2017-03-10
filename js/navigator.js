f6ed12edebbfbd4361841d6ecafe78878c9134de
Adam Hayward <achayward1@gmail.com>
2017-03-10 11:05:09 -0500

    added browser support warning for non-chrome browsers (safari can be added too if it works)

--- js/navigator.js
+++ js/navigator.js
@@ -56,3 +56,28 @@ function animateNav(parent) {
     backgroundColor: "white",
   }, 2000);
 }
+
+navigator.sayswho= (function(){
+    var ua= navigator.userAgent, tem,
+    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return M.join(' ');
+})();
+
+function recommendBrowser(){
+  if (navigator.sayswho.indexOf("Chrome") == -1) {
+    alert("This project is recommended for Chrome. " +
+      "Your browser may be incompatible.");
+    $("#browser-notice").html("This project is recommended for Chrome. " +
+      "Your browser may be incompatible.");
+  }
+}
